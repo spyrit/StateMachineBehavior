e97ee01bf1867d5f073b654a936406a1c7a5bb1c
Alexandre Cousin <alexandre.cousin@spyrit.net>
2019-02-21 13:23:45 +0100

    change or by ||

--- src/templates/objectPreSave.php
+++ src/templates/objectPreSave.php
@@ -1,4 +1,4 @@
-$isStateColumnModified = $this->isNew() or $this->isColumnModified(<?php echo $modelTableMap; ?>::<?php echo $stateColumn ?>);
+$isStateColumnModified = $this->isNew() || $this->isColumnModified(<?php echo $modelTableMap; ?>::<?php echo $stateColumn ?>);
 <?php if ($timestampable): ?>
 if ($isStateColumnModified) {
     switch ($this-><?php echo $stateColumnGetter ?>()) {
